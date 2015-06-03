239c3233f3e8fd1d5e5ff879da86ca01b460ead5
Gabe Roeloffs <gdroel@gmail.com>
2015-06-03 09:39:59 -0700

    added some docs n stuff

--- models/Validation.js
+++ models/Validation.js
@@ -10,26 +10,11 @@ var Validation = function(name, username, callback){
     // give karma to yourself, but for some reason when username is passed
     // as a parameter it becomes a weird string like: <@U04RL0M1F>
 
-    if(this.checkIfSameUsername() == false){
+    if(this.checkFirstCharacter() == false){
 
       var response = {
 
-        "text":"You can't give karma to yourself, narcissist.",
-        "parse":"none"
-
-      }
-
-      this.callback(response)
-      console.log("You can't give karma to yourself, narcissist.");
-      return false;
-
-    }
-
-    else if(this.checkFirstCharacter() == false){
-
-      var response = {
-
-        "text": "You used the wrong syntax or that person isn't on your team. Give karma like \"++ @username\""+this.name,
+        "text": "You used the wrong syntax or that person isn't on your team. Give karma like \"++ @username\"",
         "parse":"none"
       }
 
@@ -38,17 +23,18 @@ var Validation = function(name, username, callback){
       return false;
     }
 
-    else if(this.checkFirstCharacter() == true && this.checkIfSameUsername() == true){
+    else if(this.checkFirstCharacter() == true){
 
       return true;
 
     }
 
   }
-
-  //Makes sure that the karma-giver users a valid username,
-  //such as @gabe instead of saying gabe. It also magically covers a
-  //lot of different use cases, such as someone typing ++kjsdfalds
+  
+  // I have to say, this function is pretty clever. If I type in @gabe (who is in a team on slack),
+  // slack doesn't send the plaintext "@gabe." It instead sends this weird slack code
+  // formatted like <@#####> that auto hyperlinks to a user in slack. If @gabe is not on a team in slack,
+  // then slack just sends the plaintext, since slack auto hyperlinks people who are on a team.
 
   this.checkFirstCharacter = function(){
 
@@ -67,23 +53,6 @@ var Validation = function(name, username, callback){
 
       return true;
     }
-
-  }
-
-  //Makes sure someone isn't giving karma to themselves
-  this.checkIfSameUsername = function(){
-
-  //   var username = "@"+this.username;
-  //   console.log('username'+username);
-  //   console.log('name'+this.name);
-  //   if(username == this.name){
-  //     return false
-  //   }else{
-  //     return true;
-  //   }
-  // }
-
-  return true;
 
   }
 
